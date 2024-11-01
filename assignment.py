94f6cfba5ed53f68fd7b180ffaa2ef5561a977cb
Bryson-Mazzarello <bryson.mazzarello@stu.ocsb.ca>
2024-11-01 14:43:45 +0000

    final

--- assignment.py
+++ assignment.py
@@ -1,1 +1,24 @@
+def Calc(x, y):
+  if x > 0:
+    returning = (x-1)*0.75 + 1
+  else:
+    returning = 0
+  if y == "Large":
+    returning = returning + 6
+  elif y == "XL":
+    returning = returning + 10
+  returning = returning*1.13
+  return "$" + str(returning)
 
+print("Available Sizes are Large and XL")
+PS = input()
+chars = set('0123456789')
+if PS == "Large" or PS == "XL":
+  print("Topping amount is $1 for the first and afterwards $0.75 Each")
+  TPs = input()
+  if all((c in chars) for c in TPs):
+    print(Calc(float(TPs), PS))
+  else:
+    print("Invalid Toppings")
+else:
+  print("Invalid Size")
--- handler.php
+++ handler.php
@@ -1,23 +1,81 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Pizza Butt Inc</title>
+</head>
+
+<body>
+  <!-- Header start -->
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <!-- Title -->
+        <span class="mdl-layout-title">Pizza Butt Instance</span>
+        <!-- Add spacer, to align navigation to the right -->
+        <div class="mdl-layout-spacer"></div>
+        <!-- Navigation. We hide it in small screens. -->
+        <nav class="mdl-navigation mdl-layout--large-screen-only">
+          <a class="mdl-navigation__link" href="index.html">Main Page</a>
+        </nav>
+      </div>
+    </header>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">Navigation</span>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="index.html">Main Page</a>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <center>
+        <h4>So here's what it came up as!</h4>
+        <?php
+        $myVariable = "";
+        if (isset($_GET['XL'])) {
+          if (isset($_GET['XL'])) {
+            $PizzaSize = "XL";
+          }
+        } elseif (isset($_GET['Large'])) {
+          if ($_GET['Large'] == "on") {
+            $PizzaSize = "Large";
+          }
+        } else {
+          echo "<h4>Don't let me find you. It ain't hide and seek anymore. I'm gonna find you. Watch your back.</h4>\n";
+          $PizzaSize = "nil";
+        }
+        if (isset($_GET['TPs'])) {
+          $Toppings = $_GET['TPs'];
+          if ($Toppings > 4) {
+            echo "<h4>Sorry we can't add " .  $Toppings . " toppings so we 'automatically' recognised you only want four toppings</h4>\n";
+            $Toppings = 4;
+          }
+        }
+        if ($PizzaSize == "XL") {
+          if ($Toppings > 1) {
+            echo "<h4>le cost is = " .  (($Toppings - 1) * 0.75) + 10 . "</h4>\n";
+          }
+          if ($Toppings < 1) {
+            echo "<h4>le cost is = " . 10 . "</h4>\n";
+          }
+        } elseif ($PizzaSize == "Large") {
+          if ($Toppings > 1) {
+            echo "<h4>le cost is = " .  (($Toppings - 1) * 0.75) + 6 . "</h4>\n";
+          }
+          if ($Toppings < 1) {
+            echo "<h4>le cost is = " . 6 . "</h4>\n";
+          }
+        } else {
+          echo "<h4>You're cooked bud (Size Error) </h4>\n";
+        }
+        ?>
+        </center>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
